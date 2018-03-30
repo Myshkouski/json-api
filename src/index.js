826ef10b3c104116b7e7e59a8aeb05d694189f33
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2018-03-30 21:40:32 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import jsonPatch from 'json8-patch'
 import defaults from 'lodash/defaultsDeep'
-import { validate, getSchemas as _getSchemas } from './validate'
+import cloneDeep from 'lodash/cloneDeep'
+import {
+  validate,
+  getSchemas as _getSchemas
+} from './validate'
 import mapValidationErrors from './mapValidationErrors'
 
 class JsonApi {
   static async validate(...args) {
-    let ref = '/', body = args[0]
-    if(args.length > 1) {
+    let ref = '/',
+      body = args[0]
+    if (args.length > 1) {
       ref = args[0]
       body = args[1]
     }
 
     try {
       return await validate(ref, body)
-    } catch(error) {
+    } catch (error) {
       throw {
         message: `Validation error`,
         reasons: error.errors.map(mapValidationErrors)
@@ -29,6 +34,19 @@ class JsonApi {
     return jsonPatch.has(doc, path)
   }
 
+  static link(hrefOrLink) {
+    let link = {}
+    if (typeof hrefOrLink == 'string') {
+      link = hrefOrLink
+    } else if (typeof hrefOrLink == 'object') {
+      link = cloneDeep(hrefOrLink)
+    } else {
+      return null
+    }
+
+    return link
+  }
+
   static async patch(body, ops, options = {}) {
     options = defaults({}, options, {
       reversible: false
@@ -39,23 +57,23 @@ class JsonApi {
     try {
       try {
         res = jsonPatch.apply(body, ops, options)
-      } catch(error) {
+      } catch (error) {
         error.detail = `Cannot apply JSON patch`
 
         throw error
       }
 
-      if(options.validatePatch) {
+      if (options.validatePatch) {
         try {
           await JsonApi.validate(res.doc)
-        } catch(error) {
+        } catch (error) {
           error.detail = `Document validation failed after patch has been applied`
 
           throw error
         }
       }
-    } catch(error) {
-      if(options.reversible) {
+    } catch (error) {
+      if (options.reversible) {
         const reverted = jsonPatch.revert(body, res.revert).doc
 
         error.doc = reverted
@@ -70,32 +88,23 @@ class JsonApi {
   }
 
   static async add(body, path, value, options) {
-    const ops = [
-      { op: 'add', path, value }
-    ]
+    const ops = [{
+      op: 'add',
+      path,
+      value
+    }]
 
     return await JsonApi.patch(body, ops, options)
   }
 
-  static getSchemas(options) {
-    options = defaults({}, options, {
-      type: 'schema',
-      id: '$id'
-    })
-
+  static getSchemas() {
     const schemas = _getSchemas()
-    return Object.keys(schemas).reduce((array, key, index) => {
+
+    return Object.keys(schemas).reduce((array, key) => {
       const schema = {}
 
-      if(options.id == 'index') {
-        schema.id = index
-      } else {
-        schema.id = schemas[key][options.id]
-      }
-
-      schema.type = options.type
-
-      schema.attributes = schemas[key]
+      schema.id = key
+      schema.attributes = cloneDeep(schemas[key])
 
       array.push(schema)
 
@@ -105,9 +114,13 @@ class JsonApi {
 
   constructor(options = {}) {
     this.options = {}
+    this._cache = {
+      data: null,
+      included: null
+    }
 
-    if('body' in options) {
-      if(typeof options.body != 'object') {
+    if ('body' in options) {
+      if (typeof options.body != 'object') {
         throw new Error(`'options.body' should be a JSON object`)
       }
       this.body = options.body
@@ -115,8 +128,8 @@ class JsonApi {
       this.body = {}
     }
 
-    if('validatePatch' in options) {
-      if(typeof options.validatePatch != 'boolean') {
+    if ('validatePatch' in options) {
+      if (typeof options.validatePatch != 'boolean') {
         throw new Error(`'options.validatePatch' should be 'true' or 'false'`)
       }
       this.options.validatePatch = options.validatePatch
@@ -125,7 +138,7 @@ class JsonApi {
     }
   }
 
-  "get"(path) {
+  "get" (path) {
     return JsonApi.get(this.body, path)
   }
 
@@ -144,6 +157,10 @@ class JsonApi {
   async add(path, value) {
     return await JsonApi.add(this.body, path, value, this.options)
   }
+
+
+
+
 }
 
 export default JsonApi
